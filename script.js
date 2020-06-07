1deab6d967555d073d56a6b18140e8dcffff4340
Oisín Byrne <byrne.ois@gmail.com>
2020-06-07 17:46:54 +0200

    Finished the library app. Not happy with the need to manually reload to get delete and read functions to stick. moving on as enough time was spent here

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-let library = []
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDYA_KFCeGle4RKbEtyhcAoyKvLrW8J3tk",
+    authDomain: "my-library-15f9e.firebaseapp.com",
+    databaseURL: "https://my-library-15f9e.firebaseio.com",
+    projectId: "my-library-15f9e",
+    storageBucket: "my-library-15f9e.appspot.com",
+    messagingSenderId: "720770389542",
+    appId: "1:720770389542:web:b9ff175cd9cff6a69838e7"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let library = [] 
 let newBook = {}
+let docIds = []
+const database = firebase.firestore();
+
+// getting data
+
+let updateDatabase = function(){
+    database.collection('books').get().then((snapshot)=>{
+        snapshot.docs.forEach(doc => {
+            console.log(doc.data());
+            library.push(doc.data())
+            docIds.push(doc.id);
+            render();
+        })
+    })
+}
+updateDatabase();
+
+
+
+
 
 // initializes the container for the library
 let container = document.getElementById("container");
 
 
 // constructor for book objects
-function book(title, author, genre, hasRead, bin){
+function book(title, author, genre, hasRead){
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.hasRead = hasRead;
-    this.bin = bin;
 }
 
 // adds a book (object) to the library array
@@ -25,11 +57,8 @@ let getTitle = (entry) => library[entry].title;
 // returns the author for a given book in the library
 let getAuthor = (entry) => library[entry].author;
 
-// returns the author for a given book in the library
+// returns the genre for a given book in the library
 let getGenre = (entry) => library[entry].genre;
-
-// returns the author for a given book in the library
-let getbin = (entry) => library[entry].bin;
 
 // returns true if book has been read
 let checkHasRead = (entry) => library[entry].hasRead;
@@ -52,19 +81,25 @@ let contentCreation = function(category, container){
     else if (category === 'genre'){
         newContent = document.createTextNode(getGenre(entry));
     }
-    else if (category === 'bin'){
-        newContent = document.createTextNode(getbin(entry));
-    }
-
     else if (category === 'hasRead'){
             if(library[entry].hasRead){
-                newContent = document.createElement('span');
-                newContent.innerHTML = '<i class="fas fa-book-open"></i>';
+                newContent = document.createElement('button');
+                newContent.setAttribute('id','readButton'+entry)
+                newContent.setAttribute('class','readButton')
+                newContent.innerHTML = '<i id="read" class="fas fa-book-open"></i>';
             }
             else{
-                newContent = document.createElement('span');
-                newContent.innerHTML = '<i class="fas fa-book"></i>';
+                newContent = document.createElement('button');
+                newContent.setAttribute('id','readButton'+entry)
+                newContent.setAttribute('class','readButton')
+                newContent.innerHTML = '<i id = "not-read" class="fas fa-book"></i>';
             }
+    }
+    else if (category === 'bin'){
+        newContent = document.createElement('button');
+        newContent.setAttribute('id','deleteButton'+entry)
+        newContent.setAttribute('class','deleteButton')
+            newContent.innerHTML = '<i id="bin" class="fas fa-trash-alt"></i>';
     }
     
     // populates the <p> with text node
@@ -78,19 +113,14 @@ let contentCreation = function(category, container){
 let createBookDiv = function(entry){
     // creates div to house book info
     let newBookDiv = document.createElement("div");
-    newBookDiv.setAttribute('class','book-div')
-    newBookDiv.setAttribute('id','book'+entry)
+    newBookDiv.setAttribute('class','book-div');
+    newBookDiv.setAttribute('id','book'+entry);
     // creates a <p> with category specific information
     contentCreation('title',newBookDiv);
     contentCreation('author',newBookDiv);
     contentCreation('genre',newBookDiv);
     contentCreation('hasRead',newBookDiv);
-    if(checkHasRead(entry)){
-        contentCreation('bin',newBookDiv);
-    }
-    else{
-        contentCreation('bin',newBookDiv);
-    };
+    contentCreation('bin',newBookDiv);
     
     // adds the first book in the Library to the HTML doc 
     if(parseInt(entry) === 0){
@@ -104,33 +134,82 @@ let createBookDiv = function(entry){
 
 }
 
+// parent functinon to house logic for changing read status
+const readButtonSetUp = function(){
+    // changes 'hasRead' value of book object when button is clicked
+    // (uses number at the end of id to find index of book object in library)
+    const changeReadStatus = function(button){
+        button.addEventListener('click',function(){
+            let libraryIndex = parseInt(button.id.slice(10,))
+            library[libraryIndex].hasRead ^= true;
+            database.collection('books').doc(docIds[libraryIndex]).update({hasRead: library[libraryIndex].hasRead})
+            
+            // Re-renders page with change
+            render();
+        }) 
+    }
+    
+    // captures each read status button
+    let readButtons = document.querySelectorAll('.readButton');
+    readButtons.forEach(function(readButton){
+        changeReadStatus(readButton);
+    });
+    
+}
+
+// parent functinon to house logic for deleteing books
+const deleteButtonSetUp = function(){
+
+    const deleteBook = function(button){
+        button.addEventListener('click',function(){
+            // finds books position in library
+            let libraryIndex = parseInt(button.id.slice(12,))
+            // removes book from library
+            database.collection('books').doc(docIds[libraryIndex]).delete();
+            library.splice(libraryIndex,1);
+             // Re-renders page with change
+            render();
+
+        }) 
+    }
+
+    // captures each delete button
+    let deleteButtons = document.querySelectorAll('.deleteButton');
+    deleteButtons.forEach(function(deleteButton){
+        deleteBook(deleteButton);
+    });
+}
 
 // creates and populates a div for each book in the library
 let render = function(){
+    // clears the books already being shown so that their are no dupliactes
+    container.innerHTML = "";
     for (entry in library){
         createBookDiv(entry);
+        
     }
+    // call function that triggers changing rwad status
+    readButtonSetUp()
+    deleteButtonSetUp()
 }
 
+let addBook = document.forms["add-book"];
+addBook.addEventListener('submit', function(e){
+    e.preventDefault();
+    let titleValue = addBook.querySelector('input[id="inputTitle"]').value;
+    let authorValue = addBook.querySelector('input[id="inputAuthor"]').value;
+    let genreValue = addBook.querySelector('input[id="inputGenre"]').value;
+    let readValue = addBook.querySelector('input[id="inputRead"]').checked;
+    // creates a new book (object) with the values from the form
+    newBook = new book(titleValue,authorValue,genreValue,readValue);
+    // the new book (object) to the library
+    addToLibrary(newBook);
+    database.collection('books').add({
+        title: newBook.title,
+        author: newBook.author,
+        genre: newBook.genre,
+        hasRead: newBook.hasRead
+    });
+   render();
+})
 
-// example of creating a new book (object)
-newBook = new book("The Wind-up Bird Chronicle", "Haruki Murakami", "Surreall",false,"bin");
-// example of adding a new book (object) to the library
-addToLibrary(newBook);
-
-newBook = new book("The Hobbit",'J.R.R Tolkien','Fantasy',true,'bin')
-addToLibrary(newBook);
-
-
-
-render()
-
-// used to changed read status
-let x = true;
-console.log(x ^= true);
-
-let y = () => console.log('poop');
-
-
-let submitButton = document.querySelector('#submitBook')
-submitButton.addEventListener("click",y)
