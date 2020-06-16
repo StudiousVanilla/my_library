45ff0a4edb2b1c77b781408dc81a51025d3eff3e
Oisín Byrne <byrne.ois@gmail.com>
2020-06-16 19:11:48 +0200

    complete overhaul of library app - Uses firebase database - Class structure to code - Redesigned page layout and style
    * Add filtering options
    * Adjust for small/mobile displays

--- script.js
+++ script.js
@@ -10,206 +10,170 @@ var firebaseConfig = {
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+database = firebase.firestore();
 
-let library = [] 
-let newBook = {}
-let docIds = []
-const database = firebase.firestore();
-
-// getting data
-
-let updateDatabase = function(){
-    database.collection('books').get().then((snapshot)=>{
-        snapshot.docs.forEach(doc => {
-            console.log(doc.data());
-            library.push(doc.data())
-            docIds.push(doc.id);
-            render();
-        })
-    })
-}
-updateDatabase();
-
-
-
-
-
-// initializes the container for the library
-let container = document.getElementById("container");
-
-
-// constructor for book objects
-function book(title, author, genre, hasRead){
-    this.title = title;
-    this.author = author;
-    this.genre = genre;
-    this.hasRead = hasRead;
-}
-
-// adds a book (object) to the library array
-function addToLibrary(book){
-    library.push(book);
-}
-
-// returns the author for a given book in the library
-let getTitle = (entry) => library[entry].title;
-
-// returns the author for a given book in the library
-let getAuthor = (entry) => library[entry].author;
-
-// returns the genre for a given book in the library
-let getGenre = (entry) => library[entry].genre;
-
-// returns true if book has been read
-let checkHasRead = (entry) => library[entry].hasRead;
-
-// creates <p> for each relevant piece of information per book
-let contentCreation = function(category, container){
-    // initialse newContent which will be used later
-    let newContent = "";
-     // creates <p>
-    let newBookContent = document.createElement("p");
-    // sets the class of <p>
-    newBookContent.setAttribute('class',category)
-    // creates text node with title informatio
-    if(category === 'title'){
-        newContent = document.createTextNode(getTitle(entry));
+class ScreenDisplay{
+    constructor(){
     }
-    else if (category === 'author'){
-        newContent = document.createTextNode(getAuthor(entry));
-    }
-    else if (category === 'genre'){
-        newContent = document.createTextNode(getGenre(entry));
-    }
-    else if (category === 'hasRead'){
-            if(library[entry].hasRead){
-                newContent = document.createElement('button');
-                newContent.setAttribute('id','readButton'+entry)
-                newContent.setAttribute('class','readButton')
-                newContent.innerHTML = '<i id="read" class="fas fa-book-open"></i>';
+
+    contentDisplay(){
+        const addBook = document.querySelector('#open-form')
+        const submitBook = document.querySelector('#submit-book-form')
+        const closeForm  = document.querySelector('#close-form')
+
+        const content = document.querySelector('#container')
+        const form = document.querySelector('#form-container')
+
+        addBook.addEventListener('click',()=>{
+            if(content.style.display === 'none'){
+                content.style.display = 'block'
+                form.style.display = 'none'
             }
             else{
-                newContent = document.createElement('button');
-                newContent.setAttribute('id','readButton'+entry)
-                newContent.setAttribute('class','readButton')
-                newContent.innerHTML = '<i id = "not-read" class="fas fa-book"></i>';
+                content.style.display = 'none'
+                form.style.display = 'block'
             }
-    }
-    else if (category === 'bin'){
-        newContent = document.createElement('button');
-        newContent.setAttribute('id','deleteButton'+entry)
-        newContent.setAttribute('class','deleteButton')
-            newContent.innerHTML = '<i id="bin" class="fas fa-trash-alt"></i>';
-    }
-    
-    // populates the <p> with text node
-    newBookContent.appendChild(newContent);
-    // adds <p> to a container (the div for the book)
-    container.appendChild(newBookContent);
-}
+        })
 
-
-// creates a div to house the information for each book in the library
-let createBookDiv = function(entry){
-    // creates div to house book info
-    let newBookDiv = document.createElement("div");
-    newBookDiv.setAttribute('class','book-div');
-    newBookDiv.setAttribute('id','book'+entry);
-    // creates a <p> with category specific information
-    contentCreation('title',newBookDiv);
-    contentCreation('author',newBookDiv);
-    contentCreation('genre',newBookDiv);
-    contentCreation('hasRead',newBookDiv);
-    contentCreation('bin',newBookDiv);
-    
-    // adds the first book in the Library to the HTML doc 
-    if(parseInt(entry) === 0){
-        container.appendChild(newBookDiv);
-    }
-    // adds each subsequnet book so that the most recent is at the top
-    else{
-        let previousBook = document.getElementById('book'+parseInt(entry-1));
-        container.insertBefore(newBookDiv, previousBook);
-    }
-
-}
-
-// parent functinon to house logic for changing read status
-const readButtonSetUp = function(){
-    // changes 'hasRead' value of book object when button is clicked
-    // (uses number at the end of id to find index of book object in library)
-    const changeReadStatus = function(button){
-        button.addEventListener('click',function(){
-            let libraryIndex = parseInt(button.id.slice(10,))
-            library[libraryIndex].hasRead ^= true;
-            database.collection('books').doc(docIds[libraryIndex]).update({hasRead: library[libraryIndex].hasRead})
-            
-            // Re-renders page with change
-            render();
-        }) 
-    }
-    
-    // captures each read status button
-    let readButtons = document.querySelectorAll('.readButton');
-    readButtons.forEach(function(readButton){
-        changeReadStatus(readButton);
-    });
-    
-}
-
-// parent functinon to house logic for deleteing books
-const deleteButtonSetUp = function(){
-
-    const deleteBook = function(button){
-        button.addEventListener('click',function(){
-            // finds books position in library
-            let libraryIndex = parseInt(button.id.slice(12,))
-            // removes book from library
-            database.collection('books').doc(docIds[libraryIndex]).delete();
-            library.splice(libraryIndex,1);
-             // Re-renders page with change
-            render();
-
-        }) 
-    }
-
-    // captures each delete button
-    let deleteButtons = document.querySelectorAll('.deleteButton');
-    deleteButtons.forEach(function(deleteButton){
-        deleteBook(deleteButton);
-    });
-}
-
-// creates and populates a div for each book in the library
-let render = function(){
-    // clears the books already being shown so that their are no dupliactes
-    container.innerHTML = "";
-    for (entry in library){
-        createBookDiv(entry);
+        submitBook.addEventListener('click',()=>{
+            if(content.style.display === 'none'){
+                content.style.display = 'block'
+                form.style.display = 'none'
+            }
+            else{
+                content.style.display = 'none'
+                form.style.display = 'block'
+            }
+        })
         
+        closeForm.addEventListener('click',()=>{
+            if(content.style.display === 'none'){
+                content.style.display = 'block'
+                form.style.display = 'none'
+            }
+            else{
+                content.style.display = 'none'
+                form.style.display = 'block'
+            }
+        })
+
+
+
     }
-    // call function that triggers changing rwad status
-    readButtonSetUp()
-    deleteButtonSetUp()
+
 }
 
-let addBook = document.forms["add-book"];
-addBook.addEventListener('submit', function(e){
-    e.preventDefault();
-    let titleValue = addBook.querySelector('input[id="inputTitle"]').value;
-    let authorValue = addBook.querySelector('input[id="inputAuthor"]').value;
-    let genreValue = addBook.querySelector('input[id="inputGenre"]').value;
-    let readValue = addBook.querySelector('input[id="inputRead"]').checked;
-    // creates a new book (object) with the values from the form
-    newBook = new book(titleValue,authorValue,genreValue,readValue);
-    // the new book (object) to the library
-    addToLibrary(newBook);
-    database.collection('books').add({
-        title: newBook.title,
-        author: newBook.author,
-        genre: newBook.genre,
-        hasRead: newBook.hasRead
-    });
-   render();
-})
+class Form{
+    constructor(){
+    }
 
+    submitForm(){
+        const form = document.querySelector('#add-book-form');
+        form.addEventListener('submit',(e)=>{
+            e.preventDefault()
+            if(form.title.value === ""){
+                alert('You must enter at least a title')
+            }
+            else{
+                let checkedValue = 5
+                if(document.querySelector('.have-read-check:checked') === null){
+                    checkedValue = -1
+                }
+                else{
+                    checkedValue = 1
+                }
+                database.collection('books').add({
+                    title: form.title.value,
+                    author: form.author.value,
+                    genre: form.genre.value,
+                    hasRead: checkedValue
+                })
+            }
+            
+        })
+    }
+
+}
+
+class Library {
+    constructor(){
+    }
+
+    renderBookList(doc){
+        const bookList = document.querySelector('#book-list');
+    
+        let li = document.createElement('li');
+        let title = document.createElement('p')
+        let author = document.createElement('p');
+        let genre = document.createElement('p');
+        let hasRead = document.createElement('p');
+        let bin = document.createElement('p');
+    
+        li.setAttribute('data-id', doc.id);
+        title.textContent = doc.data().title;
+        author.textContent = doc.data().author;
+        genre.textContent = doc.data().genre;
+        if(doc.data().hasRead < 0){
+            hasRead.innerHTML = '<button type="button"><i class="fas fa-book"></i></button>'
+        }
+        else{
+            hasRead.innerHTML = '<button type="button"><i class="fas fa-book-open"></i></button>'
+        }
+        bin.innerHTML = '<button type="button"><i class="fas fa-trash"></i></button>'
+    
+        li.appendChild(title);
+        li.appendChild(author);
+        li.appendChild(genre);
+        li.appendChild(hasRead);
+        li.appendChild(bin);
+    
+        bookList.appendChild(li);
+
+        hasRead.addEventListener('click',()=>{
+            console.log(doc.data().hasRead)
+            database.collection('books').doc(doc.id).update({
+                hasRead: (doc.data().hasRead)*-1
+            })
+        })
+
+        bin.addEventListener('click',()=>{
+            database.collection('books').doc(doc.id).delete();
+        })
+
+
+    }
+
+    updateDatabase = function(){
+        database.collection('books').orderBy('title').onSnapshot(snapshot => {
+            let changes = snapshot.docChanges();
+            console.log(changes);
+            changes.forEach(change => {
+
+                const bookList = document.querySelector('#book-list');
+                if(change.type == 'added'){
+                    this.renderBookList(change.doc);
+                }
+                else if(change.type == 'modified'){
+                    let removedBook = bookList.querySelector('[data-id='+change.doc.id+ ']');
+                    bookList.removeChild(removedBook);
+                    this.renderBookList(change.doc);
+                }
+                else if(change.type == 'removed'){
+                    let removedBook = bookList.querySelector('[data-id='+change.doc.id+ ']');
+                    bookList.removeChild(removedBook);
+                }
+                
+            })
+        })
+    }
+
+}
+
+let screenDisplay = new ScreenDisplay()
+screenDisplay.contentDisplay()
+
+let library = new Library();
+library.updateDatabase()
+
+let form = new Form()
+form.submitForm();
